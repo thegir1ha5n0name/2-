54f783da496b20d3dc8c81133b68179e38d584dd
fariya <114010434+thegir1ha5n0name@users.noreply.github.com>
2022-10-08 06:04:20 +0300

    Add files via upload

---
+++ орртоn.py
@@ -0,0 +1,18 @@
+from random import randint
+spisok = []
+sum1 = 0
+sum2 = 0
+for i in range(15)
+    r = randint(-100, 100)
+    if r == 0
+        while r == 0:
+            r = randint(-100, 100)
+            spisok.append(r)
+print("рандомные числа:", spisok)
+for i in range(15):
+      if spisok[i] > 0:
+          sum1 = sum2 + spisok[i]
+      else:
+          sum2 = sum1 + spisok[i]
+          print(sum1)
+          print(sum2)
